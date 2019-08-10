4bc5f8096d5ab41394c194971200ee869371af2a
GeoffedUP <35282584+GeoffedUP@users.noreply.github.com>
2019-08-10 14:08:12 -0700

    test

--- app/assets/javascripts/template/hashrate-graph.js
+++ app/assets/javascripts/template/hashrate-graph.js
@@ -442,7 +442,7 @@ function generateHashrateGraph(eth, max_target_bn, ideal_block_time_seconds, tar
 
     data: {
         datasets: [{
-            label: "Current Estimated Hourly Hashrate",
+            label: "Current Estimated Hourly Hashrate test123",
             showLine: true,
             backgroundColor: chart_line_background_color,
             borderColor: chart_line_border_color,
